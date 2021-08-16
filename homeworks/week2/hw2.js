253737fccf5f0fe76927dddeb9fc35e84bda3a65
Yuta <yuta@avc.fund>
2021-08-16 20:37:00 +0800

    finish capitalize

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,22 @@
 function capitalize(str) {
-  
+	var output = ""
+
+	for (i=0;i<str.length; i++) {
+		if (i == 0) {
+			var firstCharCode = str[0].charCodeAt()
+			if (firstCharCode <= 122 && firstCharCode >= 97) {
+				var capitalizeCharCode = firstCharCode - 32 
+				var capitalizeChar = String.fromCharCode(capitalizeCharCode)
+				output += capitalizeChar
+			} else {
+				output += str[i]
+			}  
+		} else {
+			output += str[i]
+		} 
+	}
+
+	return output
 }
 
 console.log(capitalize('hello'));
